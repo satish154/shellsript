35f830caa2fa7319954626c626cefa57fcb8e596
Satish Kumar Thota <sthota5@opentext.com>
2026-07-16 23:02:52 +0530

    second com

--- hello.sh
+++ hello.sh
@@ -3,3 +3,7 @@ echo "hello"
 echo " hi hello i have joined recently "
 echo " i have retriggered this jobs "
 echo " just  say hello"
+x=ramesh
+y=satish
+echo "$x : hello $y"
+echo "$y: hey heii $x"
