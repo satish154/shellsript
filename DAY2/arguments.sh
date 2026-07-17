2b533259f9281303d5b4929a5ef1f7973511d025
Satish Kumar Thota <sthota5@opentext.com>
2026-07-17 23:29:43 +0530

    my changes creating day and day2 folders for bettr understanding

---
+++ DAY2/arguments.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#passing arguments 
+person1=$1
+person2=$2
+
+echo "$person1 :heyy hello $person2"
+echo "$person2 : hello $person1"
+
+
+#read command 
+echo "please neter your pin number "
+read -s PIN_NUMBER  #pin numbers is variable 
+echo "pin entered is $PIN_NUMBER"
