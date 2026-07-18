6d9ad288b5351b631a348ae2c7c81776983b3769
Satish Kumar Thota <sthota5@opentext.com>
2026-07-18 23:38:54 +0530

    first one

---
+++ DAY2/datatype.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+NUMBER1=20
+NUMBER2=30
+NAME=Devops
+SUM=$(($NUMBER2+$NUMBER1+$NAME))
+echo "sume is : ${SUM}"
+
+#collection of elements is called an array 
+LEADERS=("satish" "kumar" "Thota")
+echo "one leader: ${LEADERS[@]}"
+echo "one leader: ${LEADERS[1]}"
