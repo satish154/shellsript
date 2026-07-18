917e35804e2c7ca66c3f98dd426fd8633227447a
Satish Kumar Thota <sthota5@opentext.com>
2026-07-18 21:23:11 +0530

    special varibales

--- DAY2/Date.sh
+++ DAY2/Date.sh
@@ -4,6 +4,6 @@ Date=$(date)
 echo " date is : $Date"
 START_TIME=$(date +%s)
 sleep 10
-END_TIME=$(date +$s)
-TOTAL_TIME=$(($START_TIME-$END_TIME))
+END_TIME=$(date +%s)
+TOTAL_TIME=$(($END_TIME-$START_TIME))
 echo "script executed in : $TOTAL_TIME seconds"
---
+++ DAY2/specialvar.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo " all variables passe dto the script: $@"
+echo  " all variables passe dto the script: $"
+echo " script name :$0"
+echo " present working directory $PWD"
+echo  " who is runnign  : $USER"
+echo "home directory : $HOME"
