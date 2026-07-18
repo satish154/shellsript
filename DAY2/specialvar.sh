2ba5dccdc57ca0bd24207662bbfee1a2c18f3255
Satish Kumar Thota <sthota5@opentext.com>
2026-07-18 21:35:24 +0530

    special varibales

--- DAY2/specialvar.sh
+++ DAY2/specialvar.sh
@@ -5,3 +5,6 @@ echo " script name :$0"
 echo " present working directory $PWD"
 echo  " who is runnign  : $USER"
 echo "home directory : $HOME"
+echo "pid of the script :$$"
+sleep 50 &
+echo " PID of the last command in background is : $!"
