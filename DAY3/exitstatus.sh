553a3139fb967ac7043afbac1adc93ddd8e5373e
Satish Kumar Thota <sthota5@opentext.com>
2026-07-20 01:58:57 +0530

    new

--- DAY3/exitstatus.sh
+++ DAY3/exitstatus.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
-USERID= $ (id -u)
+USERID= $(id -u)
+if [ $USERID -ne 0 ]; then 
+    echo "Error: run this script root privillages"
+fi
+dnf install nginx -y
+if [ $? -ne 0 ]; then 
+    echo " error : installing nginx is failed "
+else
+    echo " succesfully installed "
