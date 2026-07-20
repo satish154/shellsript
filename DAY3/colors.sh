5b456a0d52ea29d7df4e485bd4a3f487512c47b3
Satish Kumar Thota <sthota5@opentext.com>
2026-07-21 00:11:23 +0530

    new one

--- DAY3/colors.sh
+++ DAY3/colors.sh
@@ -1,7 +1,40 @@
 #!/bin/bash
-R="\e[33m"
+R="\e[31m"
 G="\e[32m"
+Y="\e[33m"
 N="\e[0m"
 
 echo -e "$G hello world $N"
 echo " check this color "
+#!/bin/bash
+USERID=$(id -u)
+R="\e[33m"
+G="\e[32m"
+N="\e[0m"
+if [ $USERID -ne 0 ]; then 
+    echo "Error: run this script  with root privillages"
+    exit 1 #failure is other than 0
+fi
+validate(){ #functions receives inputs through args 
+    if [ $1 -ne 0 ]; then 
+    echo -e " error : installing $2 is failed "
+    exit 1
+else
+    echo  -e " $2 $G succesfully installed $n"
+fi
+}
+dnf list intalled mysql
+if ($? -ne 0); then 
+    dnf install mysql -y
+    validate $? "mysql"
+else
+    echo -e " mysql already exists.........$Y Skipping $N"
+fi
+
+dnf list intalled nginx
+    if ($? -ne 0); then 
+    dnf install nginx -y
+    validate $? "nginx"
+else
+    echo -e " nginx already exists.........$Y Skipping $N"
+fi
